b8125046b165c54f69421aa6df3c8427db30ac7c
Michael Hoitomt <mhoitomt@primedia.com>
2014-01-27 16:56:31 -0600

    [MH] [62183030] Fix for bower

--- jquery.autotagging.js
+++ jquery.autotagging.js
@@ -3,7 +3,7 @@
   var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments); }; },
     __indexOf = [].indexOf || function(item) { for (var i = 0, l = this.length; i < l; i++) { if (i in this && this[i] === item) return i; } return -1; };
 
-  define(['jquery', './lib/browserdetect', 'jquery-cookie'], function($, browserdetect) {
+  define(['jquery', 'browserdetect', 'jquery-cookie'], function($, browserdetect) {
     var WH;
     return WH = (function() {
       function WH() {
