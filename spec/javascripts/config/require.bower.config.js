e9218fabfb2f254c42ae6faf2b684b5a5739c9aa
Caleb Wright <awright@rentpath.com>
2014-02-06 15:11:29 -0500

    [CW] [00000] Jasmine Runs with Bower

---
+++ spec/javascripts/config/require.bower.config.js
@@ -0,0 +1,11 @@
+require.config({
+  shim: {
+
+  },
+  paths: {
+    browserdetect: "../../../vendor/bower/browserdetect/browserdetect",
+    jquery: "../../../vendor/bower/jquery/jquery",
+    "jquery.cookie": "../../../vendor/bower/jquery.cookie/jquery.cookie",
+    requirejs: "../../../vendor/bower/requirejs/require"
+  }
+});
