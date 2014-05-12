e8f073c78ad0ae3b49b801fa1c3612c38c889ca9
Luke Fender <lfender6445@gmail.com>
2014-05-12 11:44:56 -0400

    [LF] [000] Move jasmine-jquery to helper

--- spec/javascripts/config/bootstrap.js
+++ spec/javascripts/config/bootstrap.js
@@ -1,4 +1,11 @@
-requirejs.config({
-  waitSeconds: 120,
-  paths: { 'jasmine-jquery': 'vendor/js/jasmine-jquery' }
+var root = '../../../';
+require.config({
+  shim: {},
+  paths: {
+    browserdetect: root + 'vendor/javascripts/browserdetect/browserdetect',
+    jquery: root + 'vendor/javascripts/jquery/dist/jquery',
+    underscore: root + 'vendor/javascripts/underscore/underscore',
+    'jquery.cookie': root + 'vendor/javascripts/jquery.cookie/jquery.cookie',
+    requirejs: root + 'vendor/javascripts/requirejs/require'
+  }
 });
