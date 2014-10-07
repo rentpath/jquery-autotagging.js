e90496adbe54187585e5d14070bc6c8262bfe858
Luke Fender <lfender6445@gmail.com>
2014-10-07 16:43:36 -0400

    [LF] [000000] Fix build

--- dist/require.config.js
+++ dist/require.config.js
@@ -1,15 +1,12 @@
 require.config({
-  baseUrl: '',
-  shim: {
-
-  },
+  shim: {},
   paths: {
     requirejs: '../vendor/bower/requirejs/require',
     browserdetect: '../vendor/bower/browserdetect/browserdetect',
     jquery: '../vendor/bower/jquery/dist/jquery',
     'jquery.cookie': '../vendor/bower/jquery.cookie/jquery.cookie',
     underscore: '../vendor/bower/underscore/underscore',
-    'jquery-autotagging': '../dist/jquery-autotagging',
+    'jquery-autotagging': '../dist/shared/jquery-autotagging',
     'click-handler': '../dist/shared/click-handler',
     'select-change-handler': '../dist/shared/select-change-handler',
   },
